90cccd6c887ede57129db0295fd3970e4174bd2a
Gilles Fabre <gillesfabre34@gmail.com>
2021-04-05 12:19:24 +0200

    v2.0.0-alpha.3 TAG 210405-1209 runs with @mapper v2.0.0-alpha.21

--- projects/dist/genese-angular/lib/enums/request-method.d.ts
+++
@@ -1,7 +0,0 @@
-export declare enum RequestMethod {
-    DELETE = "delete",
-    GET = "get",
-    PATCH = "patch",
-    POST = "post",
-    PUT = "put"
-}
--- projects/dist/genese-angular/lib/genese.module.d.ts
+++
@@ -1,8 +0,0 @@
-import { ModuleWithProviders } from '@angular/core';
-import { GeneseEnvironmentService } from './services/genese-environment.service';
-import { GeneseService } from './services/genese.service';
-import { HttpClient } from '@angular/common/http';
-export declare function useFactory(http: HttpClient, geneseEnvironment: GeneseEnvironmentService): GeneseService;
-export declare class GeneseModule {
-    static forRoot(): ModuleWithProviders<GeneseModule>;
-}
